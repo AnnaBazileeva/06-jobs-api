10a404c639acbd67297d28c87dee93b34787ba96
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-05-25 03:26:18 -0400

    try to fix errors because deploiment faild

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 const start = async () => {
   try {
